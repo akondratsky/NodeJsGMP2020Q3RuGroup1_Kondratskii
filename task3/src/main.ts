1d731b11bffeafe7d4da7361f1dd373c57ae660c
aleksandr.kondratsk <aleksandr.kondratsk@ihsmarkit.com>
2020-08-24 23:26:39 +0400

    - extra files + yargs with PORT

--- task3/src/main.ts
+++ task3/src/main.ts
@@ -1,13 +1,14 @@
 import express, { Application } from 'express';
 import { init } from 'app/loaders';
 import { DEFAULT_PORT } from 'app/config';
+import { argv } from 'yargs';
 
 const startApplication = async () => {
     const app: Application = express();
 
     await init(app);
 
-    const port = DEFAULT_PORT;
+    const port = argv.PORT || DEFAULT_PORT;
 
     app.listen(port, () => {
         console.log(`App is listening on port ${port}!`);
--- task3/src/types/user.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    id: string,
-    login: string,
-    password: string,
-    age?: number,
-    isDeleted: boolean
-}
