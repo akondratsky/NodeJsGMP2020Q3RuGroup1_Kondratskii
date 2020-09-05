90e16664abc5b769ab823443c377014242196bf8
aleksandr.kondratsk <aleksandr.kondratsk@ihsmarkit.com>
2020-09-05 14:23:41 +0400

    fixed build

--- task4/src/config/variables.ts
+++ task4/src/config/variables.ts
@@ -2,5 +2,5 @@ import { argv } from 'yargs';
 
 export const PORT = argv.PORT || 4000;
 
-export const CONNECTION_STRING = argv.CONNECTION_STRING || 'postgres://eynuhexy:qcrTBGpF7uIlN1ttErKyMQKX10_xVIoN' +
+export const CONNECTION_STRING = argv.CONNECTION_STRING as string || 'postgres://eynuhexy:qcrTBGpF7uIlN1ttErKyMQKX10_xVIoN' +
     '@lallah.db.elephantsql.com:5432/eynuhexy';
