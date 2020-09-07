1e25f26fee112054346d52c948d65a3da88b7c71
aleksandr.kondratsk <aleksandr.kondratsk@ihsmarkit.com>
2020-09-08 02:34:02 +0400

    using winston to log errors

--- task4/src/services/LoggerService.ts
+++ task4/src/services/LoggerService.ts
@@ -1,23 +1,44 @@
-import { ILoggerService } from 'app/interfaces';
 import { injectable } from 'inversify';
+import { createLogger, transports, format } from 'winston';
+import { ILoggerService } from 'app/interfaces';
+import { DEBUG } from 'app/config';
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+const getText = (message: string, serializable: any) => {
+    const serialized = serializable !== undefined ? ` ${JSON.stringify(serializable)}` : '';
+    return `${message}${serialized}`;
+};
+
 @injectable()
 export class LoggerService implements ILoggerService {
+    private logger = createLogger({
+        level: 'debug',
+        format: format.combine(
+            format.timestamp(),
+            format.colorize(),
+            format.simple()
+        ),
+        transports: [
+            new transports.Console()
+        ]
+    });
+
     error(message: string, serializable?: any): Promise<void> {
-        console.error(message, serializable);
+        this.logger.error(getText(message, serializable));
         return Promise.resolve();
     }
 
     info(message: string, serializable?: any): Promise<void> {
-        console.info(message, serializable);
+        this.logger.info(getText(message, serializable));
         return Promise.resolve();
     }
 
     debug(message: string, serializable?: any): Promise<void> {
-        console.debug(message, serializable);
+        if (DEBUG) {
+            this.logger.debug(getText(message, serializable));
+        }
         return Promise.resolve();
     }
 }
