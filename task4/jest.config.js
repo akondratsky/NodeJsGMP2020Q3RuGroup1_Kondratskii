4902a8402675db6082ef0e5f0085fe846fe534b7
aleksandr.kondratsk <aleksandr.kondratsk@ihsmarkit.com>
2020-10-03 19:52:33 +0400

    + jest

---
+++ task4/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    // globalSetup: '<rootDir>/jest.setup.js',
+    moduleNameMapper: {
+        '^app/(.*)': '<rootDir>/src/$1'
+    }
+};
---
+++ task4/src/controllers/__tests__/GroupController.test.ts
@@ -0,0 +1,5 @@
+describe('hello', () => {
+    it('should do smth', () => {
+        expect(false).toBe(true);
+    });
+});
