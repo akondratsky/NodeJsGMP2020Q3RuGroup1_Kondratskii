2bb0d89cd20e81ac4e4fe2993f336152cb335e55
aleksandr.kondratsk <aleksandr.kondratsk@ihsmarkit.com>
2020-10-04 00:01:33 +0400

    initializing database for testing

--- task4/jest.config.js
+++ task4/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    // globalSetup: '<rootDir>/jest.setup.js',
+    globalSetup: '<rootDir>/jest.setup.js',
     moduleNameMapper: {
         '^app/(.*)': '<rootDir>/src/$1'
     }
--- task4/jest.setup.js
+++ task4/jest.setup.js
@@ -1,3 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { Client } = require('pg');
+const { readFileSync } = require('fs');
+
+const sql = (fileName) => readFileSync(fileName, 'utf-8');
+
 module.exports = async () => {
-    
-}
+    console.log('Starting initializing of database for testing');
+
+    const client = new Client({
+        user: 'eynuhexy',
+        host: 'lallah.db.elephantsql.com',
+        database: 'eynuhexy',
+        password: '7l8IW5q-dbASGstIQLfFZkL4g9kyWm4p'
+    });
+
+    await client.connect();
+
+    await client.query(sql('./sql/createUsers.sql'));
+    await client.query(sql('./sql/createGroups.sql'));
+    await client.query(sql('./sql/createUserGroups.sql'));
+
+    await client.end();
+
+    console.log('Database was initialized for testing');
+};
